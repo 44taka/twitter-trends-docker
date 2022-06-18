7824e4e140f6fe52858f7365bd051ee02a248e0f
yoshi_iv_44 <yoshtiaka.i.v.x.jade@gmail.com>
2022-06-19 00:01:32 +0900

    first commit

---
+++ postgres/init/init.sql
@@ -0,0 +1,3 @@
+create table if not exists woeid (id SERIAL not null, name varchar(100) not null, created_at timestamp not null default current_timestamp, updated_at timestamp not null default current_timestamp);
+create table if not exists twitter_trends (id SERIAL not null, rank smallint not null, name varchar(100) not null, url varchar(255) not null, tweet_volume integer, created_at timestamp not null default current_timestamp, updated_at timestamp not null default current_timestamp);
+create table users (id SERIAL not null, name varchar(100) not null, password varchar(10) not null);
